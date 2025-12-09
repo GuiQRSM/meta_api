3751f2a1d030306bb144762ded959b8005b9ab80
GuiQRSM <quaresmagui20@gmail.com>
2025-12-09 11:02:45 -0300

    add: new server file for the path dist

---
+++ dist/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'dotenv/config';
+export const app = express();
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+app.use(express.json());
+app.get('/', (req, res) => {
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['gub.challeng'];
+    const token = req.query['hub.verify_token'];
+    if (mode === 'subscribe' && token === verifyToken) {
+        console.log('WEBHOOK VERIFICADO');
+        res.status(200).send(challenge);
+    }
+    else {
+        res.sendStatus(403);
+    }
+});
+app.post('/', (req, res) => {
+    const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+    console.log(`\n\nWebhook recebido ${timestamp}\n`);
+    console.log(JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+});
+app.listen(port, () => {
+    console.log(`\nOuvindo na porta ${port}\n`);
+});
